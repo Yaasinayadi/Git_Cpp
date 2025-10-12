9535e2a473ed8f7958de0b6a436d6dd2368b5bb4
yaasin <ayadi.yassine@etu.uae.ac.ma>
2025-10-12 01:53:21 +0100

    Ateliers1

--- Atelier/Atelier1/exercice5.cpp
+++ Atelier/Atelier1/exercice5.cpp
@@ -1,23 +1,63 @@
 #include <iostream>
 #include <array>
 using namespace std;
+void remplir(int *arr, int n){
+    for(int i=0; i< n;i++){
+        cout << "array[" << i+1 << "]= " ;
+        cin >> *(arr + i);
+    }
+}
+void afficher(const int *arr, int n){
+    for(int i =0; i<n; i++){
+        cout <<"array[" << i+1 <<"]= "<< *(arr + i) << endl; 
+    }
+}
+int& trouverMax(int *arr, int n) {
+    int *max = arr;
+    
+    for(int i=1; i<n; i++){
+        if(*max < *(arr +i))
+            *max = *(arr +i);
+    }
+    return *max ;
+}
+void inverser(int *arr, int n){
+    int temp;
+    
+    for(int i=0; i<n/2;i++){
+        temp = arr[i]; // arr[0]
+        arr[i] = arr[n -(i+1)];// arr[0] = arr[3]
+        arr[n-(i +1)] = temp ;
+        
+    }
+}
+
+
 int main()
 {
     int n;
     int fil ;
-    // cout << sizeof(int) ;
-    int *arr = (int*) malloc(n * sizeof(int));
     cout << "Enter the taille of array: ";
     cin >> n;
+    int *arr = (int*) malloc(n * sizeof(int));
     
-    for(int i=0; i<n; i++){
-        cout << "array [" << i+1 << "] = ";
-        cin >> arr[i];
-        
-    }
     // for(int i=0; i<n; i++){
-    //     cout << "array [" << i+1 << "] = " << arr[i] << endl;
+    //     cout << "array [" << i+1 << "] = ";
+    //     cin >> arr[i];
+        
     // }
+    
+    remplir(arr, n);
+    cout << "\n------\n";
+    afficher(arr, n);
+    cout << "\n------\n";
+    int maximum = trouverMax(arr, n);
+    cout << maximum;
+    cout << "\n------\n";
+    inverser(arr, n);
+    afficher(arr,n);
+
+    free(arr);
     
     return 0;
 }
